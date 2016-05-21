0b0d0affad6ef19ab79c49ed7e624b77d6757c01
shin-nuriko <nuriko@Hyacynths-MBP.home>
2016-05-21 20:07:32 +0800

    added simple UI instead of just using console
    completed section 4 of udemy tutorial :)

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
@@ -7,6 +8,9 @@ public class NumberWizard : MonoBehaviour {
 	int min;
 	int guess;
 
+	int maxGuessAllowed = 15;
+	
+	public Text text;
 
 	// Use this for initialization
 	void Start () {
@@ -14,46 +18,37 @@ public class NumberWizard : MonoBehaviour {
 	}
 	
 	void StartGame () {
-
 		max = 1000;
 		min = 1;
-		guess = 500;
-		
-		print ("==========================");
-		print ("Welcome to Number Wizard");
-		print ("Pick a number in your head, but don't tell me.");
-		
-		print ("The highest number you can pick is " + max);
-		print ("The lowest number you can pick is " + min);
-		
-		print ("Is the number higher or lower than " +  guess + "?");
-		print ("Up = higher, down = lower, return = equal");
-		
-		max = max + 1;
-		
+		NextGuess();
+	}
+	
+	public void GuessHigher (){
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower () {
+		max = guess;
+		NextGuess();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			//print("Up arrow pressed");
-			min = guess;
-			NextGuess();
+			GuessHigher();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			//print("Down arrow pressed");
-			max = guess;
-			NextGuess();
-		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			print("I won!");
-			StartGame();
-		}
+			GuessLower();
+		} 
 	}
 	
 	void NextGuess () {
-		guess = (max + min) / 2;
-		print ("Higher or lower than " + guess);
-		print ("Up = higher, down = lower, return = equal");		
+		guess = Random.Range(min, max);
+		text.text = guess.ToString();
+		maxGuessAllowed = maxGuessAllowed - 1;
+		if (maxGuessAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
 	}
 	
 }
